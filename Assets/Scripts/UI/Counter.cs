e5b02b674b273b0e7d007657ede15874d3714695
SudanySwag <homdykalefa@gmail.com>
2026-02-12 04:39:13 -0500

    Counter that slides down at 100

---
+++ Assets/Scripts/UI/Counter.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+using System.Collections.Generic;
+using System;
+
+public class Counter : MonoBehaviour
+{
+    [SerializeField] UIDocument doc;
+
+    VisualElement root;
+    VisualElement counter;
+    Label count;
+
+    public void SetDocument(UIDocument document)
+    {
+        doc = document;
+    }
+
+    private void SetRGCCount(CellType cellType)
+    {
+        if (cellType == CellType.RGC)
+            count.text = CellManager.Instance.GetCellCount(CellType.RGC).ToString();
+    }
+
+    private void slideDown(CellType cellType) {
+        if (cellType == CellType.RGC && CellManager.Instance.GetCellCount(CellType.RGC) > 100) {
+            counter.style.top = new Length(0, LengthUnit.Percent);
+            CellManager.CellBirth -= slideDown;
+        }
+    }
+
+    public void Awake()
+    {
+        var uiDoc = doc ? doc : GetComponent<UIDocument>();
+        if (uiDoc == null)
+        {
+            Debug.LogError("Counter: No UIDocument found! Please assign a UIDocument in the Inspector or add a UIDocument component.");
+            return;
+        }
+
+        root = uiDoc.rootVisualElement;
+        if (root == null)
+        {
+            Debug.LogError("Counter: UIDocument root is null!");
+            return;
+        }
+
+        counter = root.Q<VisualElement>("Counter");
+        count = root.Q<Label>("Count");
+
+        counter.style.top = new Length(-150, LengthUnit.Percent);
+
+        CellManager.CellBirth += SetRGCCount;
+        CellManager.CellBirth += slideDown;
+    }
+
+}
